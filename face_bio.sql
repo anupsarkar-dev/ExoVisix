a0ba8ab77a581bf39677c727f34ac0d20f68494e
Anup Sarkar <aronno1107@gmail.com>
2017-11-09 22:08:15 +0530

    Update face_bio.sql

--- face_bio.sql
+++ face_bio.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2017 at 05:12 PM
+-- Generation Time: Nov 09, 2017 at 05:34 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `ghosteye`
+-- Database: `cdcol`
 --
 
 -- --------------------------------------------------------
@@ -34,11 +34,7 @@ CREATE TABLE IF NOT EXISTS `face_bio` (
   `reg` int(10) NOT NULL,
   `age` int(10) NOT NULL,
   `section` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=444 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `face_bio`
---
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -58,7 +54,7 @@ ALTER TABLE `face_bio`
 -- AUTO_INCREMENT for table `face_bio`
 --
 ALTER TABLE `face_bio`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=444;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
