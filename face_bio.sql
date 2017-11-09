f8438cdd36aa6430fb3eb06b1a3215536d113ce4
Anup Sarkar <aronno1107@gmail.com>
2017-11-09 21:51:51 +0530

    Added Database File

---
+++ face_bio.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2017 at 05:12 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ghosteye`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `face_bio`
+--
+
+CREATE TABLE IF NOT EXISTS `face_bio` (
+`id` int(11) NOT NULL,
+  `code` int(10) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `reg` int(10) NOT NULL,
+  `age` int(10) NOT NULL,
+  `section` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=444 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `face_bio`
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `face_bio`
+--
+ALTER TABLE `face_bio`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `face_bio`
+--
+ALTER TABLE `face_bio`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=444;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
